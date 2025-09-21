f5636d15118f0f408d6ec28802c594a91e8f1de4
Léa Sautjeau <lea@mac.home>
2025-09-21 14:46:06 +0200

    update

--- server.js
+++ server.js
@@ -7,20 +7,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ============================
-// Endpoint : Génération du PDF
-// ============================
+// Endpoint PDF
 app.post("/api/pdfkit", (req, res) => {
   try {
     const data = req.body;
 
-    // appelle generateReport avec les données du front
+    // On génère un nouveau doc
     const doc = generateReport(data);
 
     res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", "attachment; filename=rapporttest.pdf");
+    res.setHeader("Content-Disposition", "attachment; filename=rapport.pdf");
 
-    doc.pipe(res);
+    doc.pipe(res);   // on envoie directement le PDF au client
     doc.end();
   } catch (err) {
     console.error("❌ Erreur PDF:", err);
@@ -29,6 +27,4 @@ app.post("/api/pdfkit", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Serveur sur http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`🚀 Serveur sur http://localhost:${PORT}`));
